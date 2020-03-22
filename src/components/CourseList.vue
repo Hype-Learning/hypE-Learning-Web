25f81764f08c04e8fa2919366a5c92d61b10cab2
Ewelina Lasowy <ewelinalasowy@wp.pl>
2020-03-22 19:42:09 +0100

    added CourseList component

---
+++ src/components/CourseList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="wrapper">
+    <h1>Lista kursów</h1>
+    <ul>
+      <li v-for="course in courses" v-bind:key="course.id">
+        {{ course.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "CourseList",
+  data() {
+    return {
+      courses: []
+    };
+  },
+
+  mounted() {
+    axios
+      .get("http://localhost:3000/courses")
+      .then(courses => (this.courses = courses.data));
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.wrapper {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: flex;
+  align-items: center;
+  flex-direction: column;
+}
+ul {
+  list-style: none;
+  width: 80%;
+  height: 100%;
+}
+li {
+}
+</style>
