06db0075e887a31ac75a9db17760ce961b34bb11
Ewelina Lasowy <ewelinalasowy@wp.pl>
2020-05-06 20:02:29 +0200

    Appearance improvements to Topic

--- src/components/TopicList.vue
+++ src/components/TopicList.vue
@@ -1,62 +1,107 @@
 <template>
   <div class="wrapper">
+    <br />
     <h1>Lista tematów</h1>
+    <br />
     <v-expansion-panels :inset="true" :focusable="true" :hover="true">
       <v-expansion-panel v-for="topic in topics" v-bind:key="topic.id">
-        <v-expansion-panel-header> {{ topic.title }}</v-expansion-panel-header>
+        <v-expansion-panel-header>
+          <h2>{{ topic.title }}</h2></v-expansion-panel-header
+        >
         <v-expansion-panel-content>
-          {{ topic.description }}
+          <br />
+          <h3>Opis</h3>
+          <p>{{ topic.description }}</p>
           <br />
           <v-btn color="primary" class="mr-4">
             <a :href="topic.fileUrl"> Materiały</a>
           </v-btn>
 
-          <router-link :to="`topics/${topic.id}/edit`">
-            <v-btn color="warning" class="mr-4">
-              Edytuj temat
-            </v-btn>
-          </router-link>
-          <v-btn color="error" class="mr-4" @click="deleteTopic(topic.id)">
-            Usuń temat
-          </v-btn>
+          <br />
           <br />
           <br />
 
-          <v-file-input v-model="file" loading="true" label="Dodaj rozwiązanie">
-          </v-file-input>
-
-          <v-btn
-            color="deep-purple accent-4"
-            class="mr-4"
-            @click="addSolution(topic.id)"
+          <template
+            v-if="
+              currentUser.role === 'admin' || currentUser.role === 'instructor'
+            "
           >
-            Wyślij rozwiązanie
-          </v-btn>
-
-          <router-link :to="`topics/${topic.id}/solutions`">
-            <v-btn color="warning" class="mr-4">
-              Zobacz rozwiązania
+            <h3>Zarządzaj tematem</h3>
+            <router-link :to="`topics/${topic.id}/edit`">
+              <v-btn color="warning" class="mr-4">
+                Edytuj temat
+              </v-btn>
+            </router-link>
+            <v-btn color="error" class="mr-4" @click="deleteTopic(topic.id)">
+              Usuń temat
             </v-btn>
-          </router-link>
+            <br />
+            <br />
+          </template>
+
+          <div>
+            <template v-if="currentUser.role === 'student'">
+              <v-file-input
+                v-model="file"
+                loading="true"
+                label="Dodaj rozwiązanie"
+                color="teal"
+              >
+              </v-file-input>
+
+              <v-btn
+                color="secondary"
+                class="mr-4"
+                @click="addSolution(topic.id)"
+              >
+                Wyślij rozwiązanie
+              </v-btn>
+            </template>
+            <template
+              v-if="
+                currentUser.role === 'admin' ||
+                  currentUser.role === 'instructor'
+              "
+            >
+              <router-link :to="`topics/${topic.id}/solutions`">
+                <v-btn color="success" class="mr-4">
+                  Rozwiązania zadań studentów
+                </v-btn>
+              </router-link>
+            </template>
+          </div>
+
           <br />
-          <v-btn color="success" class="mr-4">
-            Quiz
-          </v-btn>
+          <v-spacer></v-spacer>
+          <h3>Quiz</h3>
 
-          <router-link :to="`topics/${topic.id}/quiz`">
+          <template v-if="currentUser.role === 'student'">
             <v-btn color="success" class="mr-4">
-              Utwórz quiz
+              Rozpocznij quiz
             </v-btn>
-          </router-link>
+          </template>
 
-          <router-link :to="`quiz/questions`">
-            <v-btn color="success" class="mr-4">
-              Dodaj pytanie
-            </v-btn>
-          </router-link>
+          <template
+            v-if="
+              currentUser.role === 'admin' || currentUser.role === 'instructor'
+            "
+          >
+            <router-link :to="`topics/${topic.id}/quiz`">
+              <v-btn color="success" class="mr-4">
+                Utwórz quiz
+              </v-btn>
+            </router-link>
+
+            <router-link :to="`quiz/questions`">
+              <v-btn color="secondary" class="mr-4">
+                Dodaj pytanie
+              </v-btn>
+            </router-link>
+          </template>
         </v-expansion-panel-content>
       </v-expansion-panel>
     </v-expansion-panels>
+    <br />
     <router-link :to="{ name: 'AddTopic' }">
       <v-btn color="success" class="mr-4">
         Dodaj
@@ -108,6 +153,13 @@ export default {
         .then(this.$router.push({ name: "Home" }));
     },
   },
+
+  computed: {
+    currentUser() {
+      return this.$store.getters.currentUser;
+    },
+  },
+
   mounted() {
     const userJson = localStorage.getItem("user");
     const user = JSON.parse(userJson);
